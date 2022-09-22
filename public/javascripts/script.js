b55b6ea43d59da36b15929e55cd5afe87ff99ed2
chihon <b03902123@ntu.edu.tw>
2022-09-22 13:46:14 +0800

    12th

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,23 +1,24 @@
-<script>
-    function myFunction() {
-  // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
+function myFunction() {
+    //Declare variables
+    var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
+    filter = input.value;
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
 
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-        txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
+        for (j = 0; j < 4; j++) {
+            td = tr[i].getElementsByTagName("td")[j];
+            if (td) {
+                tr[i].style.display = "none";
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                    break;
+                } 
+                
+            }
+        }
     }
-  }
 }
-</script>
